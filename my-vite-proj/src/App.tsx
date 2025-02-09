feb59f69b9f42fb1200ed460ac2d016f7b3adacc
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2025-02-09 17:59:17 +0900

    first commit & implemented the redux toolkit example by following a tutorial

---
+++ my-vite-proj/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "@reduxjs/toolkit";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<Counter />
+		</>
+	);
+}
+
+export default App;
