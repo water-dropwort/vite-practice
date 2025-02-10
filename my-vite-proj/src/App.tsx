8f5ee0679b8450024668cbf6004c3c426d4bc86d
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2025-02-10 19:21:03 +0900

    Added the example of api request with the RTK Query

--- my-vite-proj/src/App.tsx
+++ my-vite-proj/src/App.tsx
@@ -4,13 +4,25 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import "@reduxjs/toolkit";
 import { Counter } from "./features/counter/Counter";
+import { useGetPokemonByNameQuery } from "./services/pokemon";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
 
 	return (
 		<>
 			<Counter />
+			{error ? (
+				<>Oh no, there was an error</>
+			) : isLoading ? (
+				<>Loading ...</>
+			) : data ? (
+				<>
+					<h3>{data.species.name}</h3>
+					<img src={data.sprites.front_shiny} alt={data.species.name} />
+				</>
+			) : null}
 		</>
 	);
 }
--- my-vite-proj/src/app/store.ts
+++ my-vite-proj/src/app/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { pokemonApi } from "../services/pokemon";
 
 export const store = configureStore({
 	reducer: {
 		counter: counterReducer,
+		[pokemonApi.reducerPath]: pokemonApi.reducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(pokemonApi.middleware),
 });
+
+setupListeners(store.dispatch);
 
 export type RootState = ReturnType<typeof store.getState>;
 
