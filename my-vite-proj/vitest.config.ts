601abdec691ec5f6802cdea810124e6cd943e047
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2025-03-16 13:16:20 +0900

    added include setting

--- my-vite-proj/vitest.config.ts
+++ my-vite-proj/vitest.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	test: {
+		include: ["tests/**/*.test.{ts,tsx}"],
 		environment: "jsdom",
 		globals: true,
 		setupFiles: "./vitest.setup.ts",
