4b0c47d793d923ff633faae339c46ce01086af5f
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2025-04-06 16:11:27 +0900

    Move the test files into the src directory.

--- my-vite-proj/vitest.config.ts
+++ my-vite-proj/vitest.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	test: {
-		include: ["tests/**/*.test.{ts,tsx}"],
+		include: ["src/**/*.test.{ts,tsx}"],
 		environment: "jsdom",
 		globals: true,
 		setupFiles: "./vitest.setup.ts",
