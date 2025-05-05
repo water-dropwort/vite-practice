bc7ca8f5e3a1255df1e1d9c0f40fbe908257b9d7
water-dropwort <57586990+water-dropwort@users.noreply.github.com>
2025-05-05 21:15:00 +0900

    axiosのaxapterをfetchに設定しなくてもmswがリクエストをインターセプトできるようになったので、元に戻した。

--- my-vite-proj/src/features/pokemon/api/get-pokemonImage.ts
+++ my-vite-proj/src/features/pokemon/api/get-pokemonImage.ts
@@ -2,10 +2,7 @@ import axios from "axios";
 import useSWR from "swr";
 
 export async function getPokemonImageByName(name: string) {
-	const request = axios.create({
-		adapter: "fetch",
-	});
-	const res = await request.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+	const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
 	return res.data.sprites.front_shiny;
 }
 
